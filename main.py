28b5924bbbef369f0bbb51bf7933c75234928f5d
minejerik <jaanerikf@gmail.com>
2024-05-24 09:55:26 -0400

    working on user registration

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask_apscheduler import APScheduler
 from pony.orm import *
+from flask_login import LoginManager, UserMixin
+
 
 class Config:
     SCHEDULER_API_ENABLED = True
@@ -12,8 +14,48 @@ scheduler = APScheduler()
 
 db = Database()
 
-db.bind(provider="sqlite", filename="main.db")
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+
+db.bind(provider="sqlite", filename="main.db", create_db=True)
+
+db.generate_mapping(create_tables=True)
+
+set_sql_debug(True)
+
+## DB models
+
+
+class User(db.Entity, UserMixin):
+    id = PrimaryKey(int, auto=True)
+    user_id = Required(str)
+    solves = Set('Solve')
+    points = Required(int)
+    username = Required(str)
+
+
+class Solve(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    solver = Required(User)
+    challenge = Required('Challenge')
+
+
+class Challenge(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    flag = Required(str)
+    solve_count = Optional(int)
+    solves = Set(Solve)
+    points = Optional(int)
+
+
+## User Functions
+
+# Used by flask login to get the user
+@login_manager.user_loader
+def load_user(user_id):
+    with db_session:
+        return User.get(user_id=user_id)
 
 @app.route("/")
 def index():
